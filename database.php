b59af76779ec7cd7d3ca1bb7cdd99abd3bb99737
Hamzashiekh <114858991+Hamzashiekh@users.noreply.github.com>
2023-03-22 15:42:49 +0500

    SMS
    SMS

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+    $conn = mysqli_connect('localhost','root','','schoolnew') or die('connection failed');
+
+// $servername = "host";
+// $username = "schoolnew";
+// $password = "Ms5x8mZS__11";
+// $dbname = "schoolnew";
+
+// Create connection
+// $conn = new mysqli ($name, $username, $password, $dbname);
+//  $conn = mysqli_connect("$host","$username","$password","$database");
+// Check connection
+// if ($conn->connect_error) {
+    // die("Connection failed: " . $conn->connect_error);
+// }
+?>
